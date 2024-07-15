94ebe374361c8ef5857d8abf3d3c4faf991567be
Moundier <garamoundier.developer@protonmail.com>
2024-07-15 04:35:06 -0300

    chore: request filter adjustments!

--- src/main/java/com/example/demo/blueprint/security/SecurityConfig.java
+++ src/main/java/com/example/demo/blueprint/security/SecurityConfig.java
@@ -3,15 +3,14 @@ package com.example.demo.blueprint.security;
 import com.example.demo.blueprint.auth.AuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,23 +26,10 @@ public class SecurityConfig {
     SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
         return http
-        .csrf((csrf) -> csrf
-            .disable()
-        )
-        // .securityMatchers(matcher -> matcher
-            // // Toggle down
-            // .requestMatchers("/storage/**")
-            // .requestMatchers("/password/**")
-            // .requestMatchers("/user/**")
-        // )
+        .csrf((AbstractHttpConfigurer::disable))
         .authorizeHttpRequests((authz) -> authz
-            // How we secure those ones
-
             .requestMatchers("auth/**").permitAll()
-            // .requestMatchers(HttpMethod.GET, "/storage/**").hasAnyRole("USER")
-            // .requestMatchers("/password/**").hasAnyRole("USER")
-            // .requestMatchers("/user/**").hasAnyRole("USER")
-            // // Toggle up
+            .requestMatchers("/storage/**", "/password/**", "/user/**").hasAnyRole("USER")
             .anyRequest().permitAll()
         )
         .sessionManagement((session) -> session
